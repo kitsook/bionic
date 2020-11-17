5390e862bde7634b8398e7ff4fa02b846299cd73
Naman Jain <naman@squareup.com>
2020-11-17 18:59:14 -0500

    Don't hash variables in internal modules

--- bionic/code_hasher.py
+++ bionic/code_hasher.py
@@ -26,6 +26,7 @@ import warnings
 
 from .code_references import get_code_context, get_referenced_objects
 from .utils.misc import oneline
+from .utils.reload import is_internal_file
 
 
 PREFIX_SEPARATOR = b"$"
@@ -161,11 +162,21 @@ class CodeHasher:
                 self._check_and_ingest(elem, code_context)
 
         elif inspect.isroutine(obj):
-            self._ingest_raw_prefix_and_bytes(type_prefix=TypePrefix.ROUTINE)
-
-            code_context = get_code_context(obj)
-            self._check_and_ingest(obj.__defaults__, code_context)
-            self._ingest_code(obj.__code__, code_context)
+            # TODO: See if we can get the version of the module and
+            # hash the version as well.
+            if obj.__module__.startswith("bionic") or is_internal_file(
+                obj.__code__.co_filename
+            ):
+                self._ingest_raw_prefix_and_bytes(
+                    type_prefix=TypePrefix.INTERNAL_ROUTINE
+                )
+                routine_name = "%s.%s" % (obj.__module__, obj.__name__)
+                self._check_and_ingest(routine_name)
+            else:
+                self._ingest_raw_prefix_and_bytes(type_prefix=TypePrefix.ROUTINE)
+                code_context = get_code_context(obj)
+                self._check_and_ingest(obj.__defaults__, code_context)
+                self._ingest_code(obj.__code__, code_context)
 
         elif inspect.iscode(obj):
             self._ingest_raw_prefix_and_bytes(type_prefix=TypePrefix.CODE)
@@ -197,11 +208,6 @@ class CodeHasher:
         ]
         self._check_and_ingest(consts)
 
-        # TODO: We don't need to hash all the references. Like the
-        # reload functionality, we should skip internal modules.
-        #
-        # This change can either be done here, or maybe inside the
-        # `inspect.isroutine` if conditional from `self._ingest`.
         if self._should_hash_refs:
             references = get_referenced_objects(code, code_context)
             self._check_and_ingest(references)
@@ -233,6 +239,7 @@ class TypePrefix(Enum):
     ROUTINE = b"AL"
     CODE = b"AM"
     CIRCULAR_REF = b"AN"
+    INTERNAL_ROUTINE = b"AO"
     DEFAULT = b"ZZ"
 
 
